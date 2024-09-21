2cbb4c1389f28fd125f294a5e51b9b99e954e07c
pavel-turonok <pasha11081998@yandex.com>
2024-09-21 19:31:08 +0300

    fix age type check

--- homework_4.js
+++ homework_4.js
@@ -27,18 +27,20 @@ const maxAge = 60;
 //     - Если "age" больше maxAge, вывести в консоль "Keep calm and look Culture channel".
 //     - Иначе выводите "Technical work".
 
-const age = 60;
+const age = "59";
 
-if (age < minAge){
-  console.log("You don't have access cause your age is " + age + " It's less then " + minAge)
-} else if (age >= minAge && age < maxAge){
-  console.log("Welcome!")
-} else if (age > maxAge){
-  console.log("Keep calm and look Culture channel")
-} else if (isNaN(age)){
+if (isNaN(age)){
   console.log("Incorrect data type")
 } else {
-  console.log("Techical work")
+  if (age < minAge){
+      console.log("You don't have access cause your age is " + age + " It's less then " + minAge)
+    } else if (age >= minAge && age < maxAge){
+      console.log("Welcome!");
+    } else if ( age > maxAge){
+      console.log("Keep calm and look Culture channel");
+    } else {
+      console.log("Techical work");
+    }
 }
 
 // получается что единственный кейс когда выведится "Technical work" - это если age = 60, так как в условие сказано что age должен быть либо > либо < maxAge, но нигде не сказано что age должен быть равен maxAge 
